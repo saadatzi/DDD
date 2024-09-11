956b8cb67e14560becf5ef74d23ec38a27913a4a
Saadatzi <saadatzi@gmail.com>
2024-09-11 09:22:25 +0200

    fix(Json): fix json to handle infinite and nan value

--- src/SSS.Api/DependencyInjection.cs
+++ src/SSS.Api/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+
 using SSS.Api.Common.Errors;
 using SSS.Api.Common.Mapping;
 
@@ -8,7 +9,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services.AddControllers();
+        services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowNamedFloatingPointLiterals;
+            });
         services.AddSingleton<ProblemDetailsFactory, SSSProblemDetailsFactory>();
         services.AddMappings();
         return services;

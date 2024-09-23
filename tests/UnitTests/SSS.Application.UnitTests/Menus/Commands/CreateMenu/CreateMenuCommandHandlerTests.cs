8c34cb510e30fde747928f75fa4017603da9fefb
Saadatzi <saadatzi@gmail.com>
2024-09-23 07:06:16 +0200

    Feat(Test): add a unit test usig Xunit

---
+++ tests/UnitTests/SSS.Application.UnitTests/Menus/Commands/CreateMenu/CreateMenuCommandHandlerTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSS.Application.UnitTests.Menus.Commands.CreateMenu;
+
+public class CreateMenuCommandHandlerTests
+{
+    // T1 - SUT - logical component we're testing
+    // T2 - scenario - what we're testing
+    // T3 - Expected outcome - what we expect the logical compontent to do
+
+    public void HandleCreateMenuCommand_WhenMenuIsValid_ShouldCreateAndReturnMenu() // good
+    {
+        // Arrange
+        // Get hold of a valid menu
+
+        // Act
+        // Invoke the handler
+
+        // Assert
+        // 1. Validate correct menu created based on command
+        // 2. Menu added to repository
+    }
+
+    public void Test_HappyFLow() // bad
+
+}
---
+++ tests/UnitTests/SSS.Application.UnitTests/SSS.Application.UnitTests.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SSS.Api;
+
+public class SSS.Application.UnitTests
+{
+    [Fact]
+public void Test1()
+{
+    Assert.True(true);
+}
+}
